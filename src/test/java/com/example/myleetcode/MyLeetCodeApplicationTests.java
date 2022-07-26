1e0390e58af5b01eead2694010320ccb4bd36111
sunhaoa <19131228619@163.com>
2022-07-26 11:37:09 +0800

    Initial commit

---
+++ src/test/java/com/example/myleetcode/MyLeetCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myleetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLeetCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
